2bb30f0a481706d619840b5c8af2b061c59fe34f
Aris Karagiannidis <akar@miltech.gr>
2022-11-06 01:36:08 +0200

    style: set and apply formatting

--- main.cpp
+++ main.cpp
@@ -3,76 +3,79 @@
 #include <windows.h>
 
 namespace CPPSerial {
-void PrintCommState(DCB dcb) {
-  //  Print some of the DCB structure values
-  _tprintf(TEXT("\nBaudRate = %lu, ByteSize = %d, Parity = %d, StopBits = %d\n"),
-           dcb.BaudRate, dcb.ByteSize, dcb.Parity, dcb.StopBits);
-}
-}  // namespace CPPSerial
+  void PrintCommState (DCB dcb) {
+    //Print some of the DCB structure values
+    _tprintf (TEXT ("\nBaudRate = %lu, ByteSize = %d, Parity = %d, StopBits = %d\n"),
+              dcb.BaudRate,
+              dcb.ByteSize,
+              dcb.Parity,
+              dcb.StopBits);
+  }
+}   //namespace CPPSerial
 
-int main() {
-  DCB dcb;
-  HANDLE hCom;
-  BOOL fSuccess;
-  const char* PortName = "COM1";
-  TCHAR* pcCommPort = TEXT((char*)PortName);  //  Most systems have a COM1 port
+int main () {
+  DCB         dcb;
+  HANDLE      hCom;
+  BOOL        fSuccess;
+  const char* PortName   = "COM1";
+  TCHAR*      pcCommPort = TEXT ((char*)PortName);   //Most systems have a COM1 port
 
-  //  Open a handle to the specified com port.
-  hCom = CreateFile(pcCommPort, GENERIC_READ | GENERIC_WRITE,
-                    0,              //  must be opened with exclusive-access
-                    NULL,           //  default security attributes
-                    OPEN_EXISTING,  //  must use OPEN_EXISTING
-                    0,              //  not overlapped I/O
-                    NULL);          //  hTemplate must be NULL for comm devices
+  //Open a handle to the specified com port.
+  hCom = CreateFile (pcCommPort, GENERIC_READ | GENERIC_WRITE,
+                     0,               //must be opened with exclusive-access
+                     NULL,            //default security attributes
+                     OPEN_EXISTING,   //must use OPEN_EXISTING
+                     0,               //not overlapped I/O
+                     NULL);           //hTemplate must be NULL for comm devices
 
   if (hCom == INVALID_HANDLE_VALUE) {
-    //  Handle the error.
-    printf("CreateFile failed with error %lu.\n", GetLastError());
+    //Handle the error.
+    printf ("CreateFile failed with error %lu.\n", GetLastError ());
     return (1);
   }
 
-  //  Initialize the DCB structure.
-  SecureZeroMemory(&dcb, sizeof(DCB));
-  dcb.DCBlength = sizeof(DCB);
+  //Initialize the DCB structure.
+  SecureZeroMemory (&dcb, sizeof (DCB));
+  dcb.DCBlength = sizeof (DCB);
 
-  //  Build on the current configuration by first retrieving all current
-  //  settings.
-  fSuccess = GetCommState(hCom, &dcb);
+  //Build on the current configuration by first retrieving all current
+  //settings.
+  fSuccess = GetCommState (hCom, &dcb);
 
   if (!fSuccess) {
-    //  Handle the error.
-    printf("GetCommState failed with error %lu.\n", GetLastError());
+    //Handle the error.
+    printf ("GetCommState failed with error %lu.\n", GetLastError ());
     return (2);
   }
 
-  CPPSerial::PrintCommState(dcb);  //  Output to console
+  CPPSerial::PrintCommState (dcb);   //Output to console
 
-  //  Fill in some DCB values and set the com state:
-  //  57,600 bps, 8 data bits, no parity, and 1 stop bit.
-  dcb.BaudRate = CBR_57600;   //  baud rate
-  dcb.ByteSize = 8;           //  data size, xmit and rcv
-  dcb.Parity   = NOPARITY;    //  parity bit
-  dcb.StopBits = ONESTOPBIT;  //  stop bit
+  //Fill in some DCB values and set the com state:
+  //57,600 bps, 8 data bits, no parity, and 1 stop bit.
+  dcb.BaudRate = CBR_57600;    //baud rate
+  dcb.ByteSize = 8;            //data size, xmit and rcv
+  dcb.Parity   = NOPARITY;     //parity bit
+  dcb.StopBits = ONESTOPBIT;   //stop bit
 
-  fSuccess = SetCommState(hCom, &dcb);
+  fSuccess = SetCommState (hCom, &dcb);
 
   if (!fSuccess) {
-    //  Handle the error.
-    printf("SetCommState failed with error %lu.\n", GetLastError());
+    //Handle the error.
+    printf ("SetCommState failed with error %lu.\n", GetLastError ());
     return (3);
   }
 
-  //  Get the comm config again.
-  fSuccess = GetCommState(hCom, &dcb);
+  //Get the comm config again.
+  fSuccess = GetCommState (hCom, &dcb);
 
   if (!fSuccess) {
-    //  Handle the error.
-    printf("GetCommState failed with error %lu.\n", GetLastError());
+    //Handle the error.
+    printf ("GetCommState failed with error %lu.\n", GetLastError ());
     return (2);
   }
 
-  CPPSerial::PrintCommState(dcb);  //  Output to console
+  CPPSerial::PrintCommState (dcb);   //Output to console
 
-  _tprintf(TEXT("Serial port %s successfully reconfigured.\n"), pcCommPort);
+  _tprintf (TEXT ("Serial port %s successfully reconfigured.\n"), pcCommPort);
   return 0;
 }
